15f523762a7d82d0af65f22d6e1163f359a44388
AfobStudio <afolabiolutobis.aos@gmail.com>
2026-05-31 19:07:45 +0100

    Add program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+
+{
+    int i = 0;
+    char c;
+
+    char a[] = "_putchar";
+
+    while (i <= 8)
+    {
+        c = a[i];
+        _putchar(c);
+    
+        i++;
+    }
+
+
+    return(0);
+
+
+}
