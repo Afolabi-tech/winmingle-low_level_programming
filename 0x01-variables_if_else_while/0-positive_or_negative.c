a3529f3546853be923f0a250f19c8961838376f1
AfobStudio <afolabiolutobis.aos@gmail.com>
2026-05-31 19:35:53 +0100

    Add function to prints positive and negative numbers

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/** 
+ *Samuel Afolabi
+ *WinMingle Community C Training
+ *This is a program that prints whether a number is positive, negative or zero.
+ * */
+
+int main(void)
+
+{
+    int n;
+    srand(time(0));
+
+    n = rand() - RAND_MAX / 3;
+
+    if (n > 0)
+        printf("%i is positive\n", n);
+    else if (n < 0)
+        printf("%i is negative\n", n);
+    else
+        printf("%i is zero\n", n);
+
+
+    return(0);
+}
+
