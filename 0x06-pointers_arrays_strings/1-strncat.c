6c7392002ab49593eb4bc2778de44d734e3d3080
root <root@DESKTOP-TRN89PE.localdomain>
2026-06-11 06:03:35 -0700

    0-preprocessor

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,16 +1,16 @@
 #include "main.h"
 
 /**
- *  * _strncat - concatenates two strings using at most n bytes from src
- *   * @dest: destination string
- *    * @src: source string
- *     * @n: maximum number of bytes to copy from src
- *      *
- *       * Return: pointer to dest
- *        */
-char *_strncat(char *dest, char *src, int n)
+* _strncpy - concatenate strings using at most n bytes from src
+* @dest: destination string
+* @src: source string
+* @n: maximum number of bytes to copy from src
+*
+* Return: pointer to dest
+*/
+	char *_strncat(char *dest, char *src, int n);
+	
 {
-	int i = 0;
 	int j = 0;
 
 	while (dest[i] != '\0')
