6b7aded01166736f54fc7c77663c4c421d84e773
root <root@DESKTOP-TRN89PE.localdomain>
2026-06-11 05:53:52 -0700

    Implement cap_string function

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @str: string to modify
+ *
+ * Return: pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+	int i, j;
+	char sep[] = " \t\n,;.!?\"(){}";
+
+	if (str[0] >= 'a' && str[0] <= 'z')
+		str[0] -= 32;
+
+	for (i = 1; str[i] != '\0'; i++)
+	{
+		for (j = 0; sep[j] != '\0'; j++)
+		{
+			if (str[i - 1] == sep[j])
+			{
+				if (str[i] >= 'a' && str[i] <= 'z')
+					str[i] -= 32;
+				break;
+			}
+		}
+	}
+
+	return (str);
+}
