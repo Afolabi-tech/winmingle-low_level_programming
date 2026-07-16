cc446bdf1d8013a7fae08d17a70fd1744ef70c24
root <root@DESKTOP-TRN89PE.localdomain>
2026-07-16 11:56:43 +0100

    Test file - Implement 0-print_listint to print all the elements of a listint_t list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <string.h>
+
+/**
+*print_listint - function that prints sll the elements of a listint_t list
+*
+*
+*/
+
+size_t print_listint(const listint_t *h)
+{
+    size_t nodes = 0;
+
+    while (h != NULL)
+    {
+        printf ("%d\n", h->n);
+
+        nodes++;
+        h = h->next;
+    }
+
+    return (nodes);
+}
